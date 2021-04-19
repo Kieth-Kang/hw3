e15c39d74b6bd3eeed0d5a10c3551058f9324e1d
Keith Kang <keith.kang@kellogg.northwestern.edu>
2021-04-18 22:27:11 -0500

    first upload

--- noober.js
+++ noober.js
@@ -11,7 +11,44 @@ document.addEventListener('DOMContentLoaded', async function() {
   console.log(ride)
 
   // 🔥 YOUR CODE GOES HERE 🔥
-  // Write the recipe (algorithm) in the comments. Then, write the code.
-  
+
+  // Define the variables for each data point in the Object.
+  // Write the ride related information to the JavaScript console
+  let passengerFirstName = ride.passengerDetails.first
+  console.log(`Passenger's first name is ${passengerFirstName}`)
+
+  let passengerLastName = ride.passengerDetails.last
+  console.log(`Passenger's last name is ${passengerLastName}`)
+
+  let passengerPhoneNumber = ride.passengerDetails.phoneNumber
+  console.log(`Passenger's phone number is ${passengerPhoneNumber}`)
+
+  let puLocation = ride.pickupLocation.address.concat(", ", ride.pickupLocation.city, ", ", ride.pickupLocation.state, ", ", ride.pickupLocation.zip)
+  console.log(`The pickup location is ${puLocation}`)
+
+  let doLocation = ride.dropoffLocation.address.concat(", ", ride.dropoffLocation.city, ", ", ride.dropoffLocation.state, ", ", ride.dropoffLocation.zip)
+  console.log(`The dropoff location is ${doLocation}`)
+
+  // Define the level of service related variables.
+  let purple = ride.purpleRequested
+  let nOfPassengers = ride.numberOfPassengers
+
+  // Figure out which level of service should be displayed, store it in memory
+  let serviceLevel
+  if (purple === true) {
+    serviceLevel = `Noober Purple`
+  } else if (nOfPassengers > 3) {
+    serviceLevel = `Noober XL`
+  } else {
+    serviceLevel = `Noober X`
+  }
+  console.log(`Service level is ${serviceLevel}`)
+
+  // Create a sentence that displays the information of the ride
+  let sentence = `${serviceLevel} Passenger: ${passengerFirstName} ${passengerLastName} - ${passengerPhoneNumber}. Pickup at ${puLocation}. Drop-off at ${doLocation}`
+
+  // Write the sentence to the JavaScript console
+  console.log(sentence)
+
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
